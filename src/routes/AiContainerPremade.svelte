227d676f8d7e2d7882235e43a41c8a765abc98a5
Lukas <lovretic2002@gmail.com>
2024-05-19 14:01:06 +0200

    biraju se 2 random pitanja iz baze pitanja (harcoded u varijabli trenutno). Na klik gumba 'drugi primjeri' se odaberu random druga dva pitanja. Na klik na neko pitanje se stavi tekst tog pitanja u input

--- src/routes/AiContainerPremade.svelte
+++ src/routes/AiContainerPremade.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
+  import IdeaIcon from "$lib/icons/IdeaIcon.svelte";
+  import { slide } from "svelte/transition";
+  import { createEventDispatcher } from "svelte";
+
   export let question: string;
+
+  const dispatch = createEventDispatcher();
 </script>
 
-<button>{question}</button>
+<button
+  type="button"
+  in:slide={{ duration: 300, delay: 0 }}
+  out:slide={{ duration: 300, delay: 150 }}
+  on:click={() => {
+    dispatch("press", { question });
+  }}
+>
+  <IdeaIcon />
+  <span>
+    {question}
+  </span>
+</button>
 
 <style lang="scss">
   @import "$lib/styleVariables.scss";
 
   button {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
     padding: 0.75rem 1.5rem;
     border: none;
     border-radius: 0.5rem;
